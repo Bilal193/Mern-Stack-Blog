cdc754d7e6a4303720fe6e77ec8f7915af3dc090
Arslan Nasir <arslan.9467882@talmeez.pk>
2024-03-04 17:16:41 +0500

    Dashboard completed

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
 import DashUsers from '../components/DashUsers';
 import DashComments from '../components/DashComments';
+import DashboardComponent from '../components/DashboardComponent';
 
 
 export default function Dashboard() {
@@ -34,6 +35,8 @@ export default function Dashboard() {
       { tab === 'users' && <DashUsers />}
       {/* comments */}
       { tab === 'comments' && <DashComments />}
+      {/* Dashboard component */}
+      {(tab === 'dash' || !tab) && <DashboardComponent />}
     </div>
   )
 }
